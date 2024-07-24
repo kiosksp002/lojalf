8f639760c30186d01585ac28d13d8d8a77248a0d
cet <renatom@cetsp.com.br>
2024-07-24 12:33:03 -0300

    V1

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa2
+ssh-add ~/.ssh/id_rsa3
 ssh -T git@github.com
 git remote -v
 git remote set-url origin git@github.com:kiosksp002/lojalf.git
